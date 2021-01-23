06b0bb68dfda4eedcdb4405718949022b38024af
BooShenWaa <a.bougouneau@gmail.com>
2021-01-23 19:48:49 +0000

    First

---
+++ src/components/Form/Form.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+
+function Form({ inputText, setInputText, todos, setTodos }) {
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value);
+  };
+  const submitToDoHandler = (e) => {
+    e.preventDefault();
+    setTodos([...todos, { text: inputText, completed: false, id: uuidv4() }]);
+    setInputText("");
+  };
+  return (
+    <div>
+      <form>
+        <input
+          onChange={inputTextHandler}
+          type="text"
+          className="todo-input"
+          value={inputText}
+        />
+        <button
+          onClick={submitToDoHandler}
+          className="todo-button"
+          type="submit"
+        >
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <div className="select">
+          <select name="todos" className="filter-todo">
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="uncompleted">Uncompleted</option>
+          </select>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Form;
